adae3dff4f015cb92f234e26137857c1253cc523
Paulo Silva <pauloalmeida1288@gmail.com>
2023-03-10 21:04:02 +0000

    Ex.1 feito

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine(); //pede um número(tipo string) à consola
+            int Number;
+
+            bool NumberVerification= int.TryParse(str, out Number); //transforma essa número(tipo string) em um número inteiro
+
+            Number++; //adiciona 1 ao número
+            Console.WriteLine(Number); //foi adicionado fora porque adiciona 1 ao número mas não mostra de imediato
+            --Number; //retira 1 ao número
+            Console.WriteLine(--Number); //foi retirado uma segunda vez porque o número voltou ao normal, sendo assim para ser menor tive que retirar mais uma vez
+        }
+    }
+}
