9d9d2a87ed85618549f77f03e52d67a18979f159
Paulo Silva <pauloalmeida1288@gmail.com>
2023-03-14 18:45:48 +0000

    Exercício 6 concluído

---
+++ Specials/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Sbyte Max Value = {sbyte.MaxValue} and Min Value = {sbyte.MinValue}");
+            Console.WriteLine($"Short Max Value = {short.MaxValue} and Min Value = {short.MinValue}");
+            Console.WriteLine($"Int Max Value = {int.MaxValue} and Min Value = {int.MinValue}");
+            Console.WriteLine($"Long Max Value = {long.MaxValue} and Min Value = {long.MinValue}");
+            Console.WriteLine($"Byte Max Value = {byte.MaxValue} and Min Value = {byte.MinValue}");
+            Console.WriteLine($"Ushort Max Value = {ushort.MaxValue} and Min Value = {ushort.MinValue}");
+            Console.WriteLine($"Uint Max Value = {uint.MaxValue} and Min Value = {uint.MinValue}");
+            Console.WriteLine($"Ulong Max Value = {ulong.MaxValue} and Min Value = {ulong.MinValue}");
+            Console.WriteLine($"Char Max Value = {char.MaxValue} and Min Value = {char.MinValue}");
+        }
+    }
+}
