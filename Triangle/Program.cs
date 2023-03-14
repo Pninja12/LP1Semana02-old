f83875508cb3b6742d99f5590a6e59decf8349e5
Paulo Silva <pauloalmeida1288@gmail.com>
2023-03-14 20:52:07 +0000

    Exercício 10 concluído

---
+++ Triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreve a quantidade -> ");
+            string quant_str = Console.ReadLine();
+            int quant;
+            int.TryParse(quant_str, out quant);
+
+            Console.Write("Escreve o carácter -> ");
+            string char_str = Console.ReadLine();
+
+            for (int i = 1; i < (quant + 1); i ++) //corre em um loop que só acaba quando atingir o número dado pelo utilizador
+            {
+                for (int j = 0; j < i; j++)//corre em loop de forma a mostrar a variável "char_str" as vezes necessárias. 
+                //quando acaba um loop, o próximo vai ser rodado mais uma vez que o anterior por causa do "j < i" já que "i" está sempre a crescer e o "j" volta sempre a zero
+                {
+                    Console.Write(char_str);
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
