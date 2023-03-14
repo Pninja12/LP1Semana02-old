b7283eb17093f0c1af5e8be0efec178da8a88800
Paulo Silva <pauloalmeida1288@gmail.com>
2023-03-14 20:11:26 +0000

    Exercício 8 concluído

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write something -> ");
+            string question = Console.ReadLine();
+
+
+            string response = question switch
+            {
+                "How are you?" => response = "I'm fine, thank you",
+
+                "What's your name?" => response = "Gran'pa",
+
+                "What's your mission?" => response = "Give love to everyone",
+
+                _ => "Carambolas, ainda não sei essa(s) palavra(s)",
+            };
+
+            Console.Write(response);
+        }
+    }
+}
